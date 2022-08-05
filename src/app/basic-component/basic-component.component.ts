1eb0d6599230a3bc9468a13936660f5e8f4ceacd
Viplav Patil <viplav.patil@nagarro.com>
2022-08-05 15:52:05 +0530

    basic-component added

---
+++ src/app/basic-component/basic-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-component',
+  templateUrl: './basic-component.component.html',
+  styleUrls: ['./basic-component.component.css']
+})
+export class BasicComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
