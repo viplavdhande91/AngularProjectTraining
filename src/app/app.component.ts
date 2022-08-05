f2c7e656468f1d496f8401cf4d3fca32bfffab78
Viplav Patil <viplav.patil@nagarro.com>
2022-08-05 17:13:35 +0530

    basic pipes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'handson-project';
-  myname = 'Viplav';
+  title = 'basicPipes';
+  myname = 'viplav';
   myrank = 101;
   student = { name: 'viplav', rank: 23 };
+  birthday = new Date(1988, 3, 15); // April 15, 1988 -- since month parameter is zero-based
+
 }
