b3598166df6a666a4a2a951c816e0a3038852b3a
Viplav Patil <viplav.patil@nagarro.com>
2022-08-09 16:41:31 +0530

    directives done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'Property Binding';
-  name = "Peter";
-  statusOfDisabled = false; //CHECK FOR BOTH VALUES true/false
+
+
+  title = 'If-else Condition';
+  show = true;
+
+  color = "blue";
+
+  color1 = "red";
+
+  users = [
+    { name: 'Viplav', phone: '1111', email: 'viplav@test.com', accounts: ['facebook', 'gmail', 'linkedin'] },
+    { name: 'Bruce', phone: '2299', email: 'bruce@test.com', accounts: ['Gmail', 'gmail', 'youtube'] },
+
+  ]
+
+
+  multiClasses = {
+    class1: true,
+    class2: false,
+    class3: true
+  };
+
+  multiStyles = {
+    'background': 'red',
+    'border': '10px solid green'
+  };
 
 }
