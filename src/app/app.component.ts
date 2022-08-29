7b612e6a2cda569d616c70c533ecf097781ddb03
Viplav Patil <viplav.patil@nagarro.com>
2022-08-29 20:38:19 +0530

    api call updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,30 @@
-
-
 import { Component } from '@angular/core';
 import { WeatherDataServiceService } from './services/weather-data-service.service';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
-  weather: any;
+  weather: any= { "product" : "" , "init" : "" , "dataseries" : []}
+  myJSON: any;
 
   constructor(private weatherData: WeatherDataServiceService) {
-    this.weatherData.weatherData().subscribe((data) => {  ///subscribe() is the function which 
+
+    this.weatherData.weatherData().subscribe((data) => {  
+      ///subscribe() is the function which 
       //passes data to only required component[For E.g app component here]
-
-
       console.log(data);
       this.weather = data;
 
     });
+
   }
+
+
+
+
+ 
+
 }
