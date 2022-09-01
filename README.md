fa968f1449492e4d84ae196926f07cda434c977a
Viplav Patil <viplav.patil@nagarro.com>
2022-09-01 20:30:31 +0530

    reactive forms finished

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# Reactive Forms
+ 
+
+
+## A)Creation Steps
+
+#### 1. Import ReactiveFormsModule in app.module.ts and register it in imports section.
+```
+import {ReactiveFormsModule} from '@angular/forms'
+
+
+ imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ]
+```
+
+
+#### 2. Import FormControl,FormGroup in in app.component.ts.
+```
+import {FormControl,FormGroup} from '@angular/forms'
+
+```
+
+#### 3. Create formobject in app.component.ts using FormGroup and Formcontrol.
+
+
+
+## A)Validation Steps
+
+#### 1. Import Validators in app.component.ts.
+```
+import { Validator} from '@angular/forms'
+
+```
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
 
 
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup ,Validator, Validators} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'handson-project';
-  myname = 'Viplav';
 
-  student = { name: 'viplav', rank: 23 };
+
+  loginform = new FormGroup({
+
+    user: new FormControl('',[Validators.required,Validators.email]),
+
+    password: new FormControl('')
+  })
+
+
+  getFormData()
+  {
+    console.log(this.loginform.value);
+  }
+   
+
+  get userData()    //WE CAN DEFINE GETTERS FOR user field
+  {
+    return this.loginform.get('user');
+  }
 }
