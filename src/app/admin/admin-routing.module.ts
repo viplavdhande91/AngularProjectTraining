d482dddc10451398a1055b65113935f83cb3d47f
Viplav Patil <viplav.patil@nagarro.com>
2022-09-02 13:45:47 +0530

    lazy loading done 2/9/2022

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from './login/login.component'
+import { LoginComponent } from './login/login.component'
 
-import {ListComponent} from './list/list.component'
+import { ListComponent } from './list/list.component'
 
-const routes: Routes = [{path: 'login' , component: LoginComponent},
+const routes: Routes = [{ path: 'login', component: LoginComponent },
 
- {path: 'list' , component: ListComponent}
+{ path: 'list', component: ListComponent }
 
 ];
 
