21a468f4c4283fc7cd6d1fe40554da69ebb31d6c
Viplav Patil <viplav.patil@nagarro.com>
2022-09-20 17:26:57 +0530

    storage added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,46 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent  {
+  ngOnInit():void{
+    console.log('This is tut 20');
+    let impArray = ['adrak', 'pyaz', 'bhindi'];
+
+    /**
+     * 
+     * 
+     * { sbusbidyid : {feepercent : 100 ,floorspcae = 34,subsidynumber :absc }   ,
+     { sbusbidyid : {feepercent : 100 ,floorspcae = 34}   
+     ,{ sbusbidyid : {feepercent : 100 ,floorspcae = 34}   
+     ,{ sbusbidyid : {feepercent : 100 ,floorspcae = 34}   ,
+     * }
+     * 
+     */
+    // // Add a key-value pair inside local Storage
+    localStorage.setItem('Name', 'Harry');
+    localStorage.setItem('Name2', 'Rohan');
+    localStorage.setItem('Sabzi', JSON.stringify(impArray));
+
+
+    sessionStorage.setItem('Name', 'Harry');
+    sessionStorage.setItem('Name2', 'ajay');
+    sessionStorage.setItem('Sabzi', JSON.stringify(impArray));
+    
+    // Clears the entire local storage
+    // localStorage.clear();
+    
+    // Clear a particular key-value pair
+    // localStorage.removeItem('Name2');
+    
+    // Retrieve an item from the local Storage
+    let name = localStorage.getItem('Name');
+    // name = JSON.parse(localStorage.getItem('Sabzi'));
+    console.log(name)
+    
+    // sessionStorage.setItem('sessionName', 'sHarry');
+    // sessionStorage.setItem('sessionName2', 'sRohan');
+    // sessionStorage.setItem('sessionSabzi', JSON.stringify(impArray));
+    
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FilterComponent } from './filter/filter.component';
-import { ReduceComponent } from './reduce/reduce.component';
 import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilterComponent,
-    ReduceComponent
+    
   ],
   imports: [
     BrowserModule,
