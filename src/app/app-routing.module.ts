d4582ab6753fe659ead78669776d82b46309a8b7
Viplav Patil <viplav.patil@nagarro.com>
2022-11-14 12:39:21 +0530

    Main updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {FilterComponent} from './filter/filter.component'
 
-import {ReduceComponent} from './reduce/reduce.component'
 
-const routes: Routes = [{component:FilterComponent ,path:'filter'},{component:ReduceComponent ,path:'reduce'}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
