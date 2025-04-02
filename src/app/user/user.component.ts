6e338c01d86bab2e9d1e88ba30f85a820d5d4914
Viplav Patil <viplavdhande91@gmail.com>
2025-04-02 23:20:14 +0530

    Changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,21 +1,13 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
-
-
-
-
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   @Output() outputSend = new EventEmitter<string>(); //string because we have to pass string data
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
