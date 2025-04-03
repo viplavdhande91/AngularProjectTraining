15e08ada2516e01bf30c574d6c7beda575c49be0
Viplav Patil <viplavdhande91@gmail.com>
2025-04-03 10:09:44 +0530

    Formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,34 @@
-
-
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
-
-
   title = 'If-else Condition';
   show = true;
 
-  color = "blue";
-
+  color = 'blue';
 
   users = [
     { name: 'Viplav', phone: '1111', email: 'viplav@test.com' },
     { name: 'Bruce', phone: '2299', email: 'bruce@test.com' },
-
-  ]
-
+  ];
 
   multiClasses = {
     class1: true,
     class2: false,
-    class3: true
+    class3: true,
   };
 
   multiStyles = {
-    'background': 'rebeccapurple',
-    'border': '2px solid yellow',
-    'color': '#fff',
-    'height': '100px'
+    background: 'rebeccapurple',
+    border: '2px solid yellow',
+    color: '#fff',
+    height: '100px',
   };
-//ngModel is 
- componentDirective = "@Component({selector: 'app-root',templateUrl: './app.component.html',styleUrls: ['./app.component.css']})";
-
+  //ngModel is
+  componentDirective =
+    "@Component({selector: 'app-root',templateUrl: './app.component.html',styleUrls: ['./app.component.css']})";
 }
